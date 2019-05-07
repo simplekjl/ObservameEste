ecf9b8371545d049ceaab5df062c291e89def2fe
Jose L Crisostomo <jl.cs606@gmail.com>
2019-05-06 22:47:19 -0700

    Concurrency and Threads
    Simple example of the ObserveOn and SubscribeOn options

--- app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
+++ app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
@@ -6,10 +6,43 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Scanner;
+
 import io.reactivex.Observable;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
+
+/**
+ * Note: Different types of Schedulers available in RxJava
+ *
+ * Schedulers.io() :
+ *
+ * This can have a limitless thread pool. Used for non CPU intensive tasks. Such as database interactions, performing network  communications and interactions with the file system.
+ *
+ * AndroidSchedulers.mainThread()
+ *
+ * This is the main thread or the UI thread. This is where user interactions happen
+ *
+ * Schedulers.newThread()
+ *
+ * This scheduler creates a new thread for each unit of work scheduled.
+ *
+ * Schedulers.single()
+ *
+ * This scheduler has a single thread executing tasks one after another following the given order.
+ *
+ * Schedulers.trampoline()
+ *
+ * This scheduler executes tasks following first in, first out basics.We use this when implementing recurring tasks.
+ *
+ * Schedulers.from(Executor executor)
+ *
+ * This creates and returns a custom scheduler backed by the specified executor.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myApp";
@@ -26,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
         mtextView = findViewById(R.id.my_text);
 
         myObservable = Observable.just(mTestString);
+
+
         // the observer has to be subscribe to get the data from the observable
         myObserver = new Observer<String>() {
             @Override
@@ -54,7 +89,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "onComplete: ");
             }
         };
+        // in the following line we are Observing the actions for the dispatcher on the main thread and running the
+        // operation from the io thread
         // subscriptions to the observer
-        myObservable.subscribe(myObserver);
+        myObservable.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(myObserver);
     }
 }
