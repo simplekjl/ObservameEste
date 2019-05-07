ed3365f23579c809ef08cfa136b7e65aae562c1d
Jose Luis Crisostomo <jl.cs606@gmail.com>
2019-05-07 13:56:37 -0700

    Creating a cleaner code
    Taking advantages of the Rx patterns to build shorter blocks of code

--- app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
+++ app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
@@ -3,19 +3,22 @@ package com.simplekjl.rxdemoimplementations;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myApp";
     private String mTestString = "Hello from RxJava";
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
     private Observable<String> myObservable;
-    private Observer<String> myObserver;
+    private DisposableObserver<String> myObserver;
+    private DisposableObserver<String> myObserver2;
     private TextView mtextView;
 
     @Override
@@ -26,27 +29,18 @@ public class MainActivity extends AppCompatActivity {
         mtextView = findViewById(R.id.my_text);
 
         myObservable = Observable.just(mTestString);
-        // the observer has to be subscribe to get the data from the observable
-        myObserver = new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
 
-                Log.i(TAG, "onSubscribe: ");
-
-            }
-
+        myObserver = new DisposableObserver<String>() {
             @Override
             public void onNext(String s) {
                 Log.i(TAG, "onNext: ");
                 // setting the value when the Observable dispatches it
                 mtextView.setText(s);
-
             }
 
             @Override
             public void onError(Throwable e) {
                 Log.i(TAG, "onError: ");
-
             }
 
             @Override
@@ -54,7 +48,39 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "onComplete: ");
             }
         };
-        // subscriptions to the observer
-        myObservable.subscribe(myObserver);
+        myObserver2 = new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.i(TAG, "onNext: ");
+                // launching a toast
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete: ");
+            }
+        };
+        // Rx Java allow to write very small blocks of code that allow you to work faster and cleaner since the
+        // code becomes readable and effective with the right use.
+        // Lets start Adding to the ComposableDisposable our first Observer which is going to be performing it's
+        // logic on the io() thread and observing in the Mainthread
+        compositeDisposable
+                .add(myObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(myObserver));
+        // adding the second observer and subscribing to the composite disposable
+        compositeDisposable
+                .add(myObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(myObserver2));
+
     }
 }
