968fc6982aa67f0426ab748d3f92a7a742b3f4cf
Jose Luis Crisostomo <jl.cs606@gmail.com>
2019-05-07 18:00:33 -0700

    range operator
    basic example of the range operator

--- app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
+++ app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private String[] greetings = {"Hola", ",", "Welcome to RxAndroid, +", " RxJava"};
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
     // sincce we are going to emit one string at the time we have to change the value the observable emits
-    private Observable<String> myObservable;
-    private DisposableObserver<String> myObserver;
+    private Observable<Integer> myObservable;
+    private DisposableObserver<Integer> myObserver;
     private TextView mtextView;
 
     @Override
@@ -29,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
         mtextView = findViewById(R.id.my_text);
 
-        // we are changing from 'just' to 'fromArray' which is going to emit one string at the time when we subscribe
-        // to it.
-        myObservable = Observable.fromArray(greetings);
+        // Range operator allows you to iterate a certain number of times given
+        // in this case I will start from 12 and increase the value 8 times
+        myObservable = Observable.range(12,8);
 
 
         // Rx Java allow to write very small blocks of code that allow you to work faster and cleaner since the
@@ -47,17 +47,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private DisposableObserver<String> getSingleItemObserver() {
-        myObserver = new DisposableObserver<String>() {
+    private DisposableObserver<Integer> getSingleItemObserver() {
+        myObserver = new DisposableObserver<Integer>() {
             @Override
-            public void onNext(String s) {
+            public void onNext(Integer number) {
                 Log.i(TAG, "onNext: ");
-                // since the fromArray operator will send one string at the time we proceed
+                // since the Ragne operator will send one number at the time we proceed
                 // to add values in the textView
-                mtextView.append(s);
+                mtextView.append(String.valueOf(number));
                 mtextView.append(" ");
                 // let's make a toast to see it work in a different way
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, String.valueOf(number), Toast.LENGTH_SHORT).show();
             }
 
             @Override
