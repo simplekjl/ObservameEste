bbf5c8dd2c9d770b24f0531343ba91036b907aac
Jose Luis Crisostomo <jl.cs606@gmail.com>
2019-05-07 17:24:46 -0700

    Just operator example
    basic example how to use the just operator

--- app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
+++ app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
@@ -14,10 +14,11 @@ import io.reactivex.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myApp";
-    private String mTestString = "Hello from RxJava";
+    private String[] greetings = {"Hola", ",", "Welcome to RxAndroid, +", " RxJava"};
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
-    private Observable<String> myObservable;
-    private DisposableObserver<String> myObserver;
+    private Observable<String[]> myObservable;
+    private Observable<String> myObservableArray;
+    private DisposableObserver<String[]> myObserver;
     private DisposableObserver<String> myObserver2;
     private TextView mtextView;
 
@@ -28,44 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
         mtextView = findViewById(R.id.my_text);
 
-        myObservable = Observable.just(mTestString);
+        myObservable = Observable.just(greetings);
+        // let's test same string but know as an array
+        myObservableArray = Observable.just("Hola", ",", "Welcome to RxAndroid, +", " RxJava");
 
-        myObserver = new DisposableObserver<String>() {
-            @Override
-            public void onNext(String s) {
-                Log.i(TAG, "onNext: ");
-                // setting the value when the Observable dispatches it
-                mtextView.setText(s);
-            }
 
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "onError: ");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i(TAG, "onComplete: ");
-            }
-        };
-        myObserver2 = new DisposableObserver<String>() {
-            @Override
-            public void onNext(String s) {
-                Log.i(TAG, "onNext: ");
-                // launching a toast
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "onError: ");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i(TAG, "onComplete: ");
-            }
-        };
         // Rx Java allow to write very small blocks of code that allow you to work faster and cleaner since the
         // code becomes readable and effective with the right use.
         // Lets start Adding to the ComposableDisposable our first Observer which is going to be performing it's
@@ -74,13 +42,63 @@ public class MainActivity extends AppCompatActivity {
                 .add(myObservable
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(myObserver));
+                        .subscribeWith(getSingleItemObserver()));
         // adding the second observer and subscribing to the composite disposable
         compositeDisposable
-                .add(myObservable
+                .add(myObservableArray
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .subscribeWith(myObserver2));
+                        .subscribeWith(getArrayObserver()));
 
+    }
+
+    private DisposableObserver<String[]> getSingleItemObserver() {
+        myObserver = new DisposableObserver<String[]>() {
+            @Override
+            public void onNext(String[] s) {
+                Log.i(TAG, "onNext: ");
+                // since the just operator will send the data as it is we will get the array at once so we iterate on it
+                // to add values in the textView
+                for (String note : s) {
+                    mtextView.append(note);
+                    mtextView.append(" ");
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete: ");
+            }
+        };
+        return myObserver;
+    }
+
+    private DisposableObserver<String> getArrayObserver() {
+
+        myObserver2 = new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.i(TAG, "onNext: ");
+                // passing several objects to the just operator will end up sending each item individually
+                Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete: ");
+            }
+        };
+        return myObserver2;
     }
 }
