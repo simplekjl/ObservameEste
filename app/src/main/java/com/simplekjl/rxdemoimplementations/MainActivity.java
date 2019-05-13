6282944620a075ccecf2b67b92c582990f78a1ba
Jose L Crisostomo <jl.cs606@gmail.com>
2019-05-12 21:46:42 -0700

    Rx Android

--- app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
+++ app/src/main/java/com/simplekjl/rxdemoimplementations/MainActivity.java
@@ -1,60 +1,85 @@
 package com.simplekjl.rxdemoimplementations;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import io.reactivex.Observable;
-import io.reactivex.Observer;
+import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myApp";
-    private String mTestString = "Hello from RxJava";
-    private Observable<String> myObservable;
-    private Observer<String> myObserver;
-    private TextView mtextView;
+
+    // composite disposable
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    // views
+    private TextView content;
+    private EditText input;
+    private Button clearButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // getting the viws
 
-        mtextView = findViewById(R.id.my_text);
+        content = findViewById(R.id.content);
+        input = findViewById(R.id.inputText);
+        clearButton = findViewById(R.id.clearBtn);
 
-        myObservable = Observable.just(mTestString);
-        // the observer has to be subscribe to get the data from the observable
-        myObserver = new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
+        //Rx Android reduces de boiler plate and the inconsistency of the code
+        //talke a look to the normal implementation of a text watcher
 
-                Log.i(TAG, "onSubscribe: ");
+//        input.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                    content.setText(s);
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//
+//            }
+//        });
+//
+//        clearButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                    input.setText("");
+//                    content.setText("");
+//            }
+//        });
 
-            }
+        // RxAndroid Approach
+        Disposable inputDisposable = RxTextView.textChanges(input)
+                .subscribe(charSequence -> content.setText(charSequence));
+        Disposable buttonDisposable = RxView.clicks(clearButton)
+                .subscribe(o -> {
+                    input.setText("");
+                    content.setText("");
+                });
 
-            @Override
-            public void onNext(String s) {
-                Log.i(TAG, "onNext: ");
-                // setting the value when the Observable dispatches it
-                mtextView.setText(s);
+        compositeDisposable.add(inputDisposable);
+        compositeDisposable.add(buttonDisposable);
+    }
 
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.i(TAG, "onError: ");
-
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i(TAG, "onComplete: ");
-            }
-        };
-        // subscriptions to the observer
-        myObservable.subscribe(myObserver);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
     }
 }
